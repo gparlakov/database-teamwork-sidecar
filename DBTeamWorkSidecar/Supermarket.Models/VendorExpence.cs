8f1aff376b83adb598d31b5bb7eb827a779e6d00
ivovachkov <ivaylo.l.vachkov@gmail.com>
2013-07-22 11:19:14 -0700

    VendorExpence class add

---
+++ DBTeamWorkSidecar/Supermarket.Models/VendorExpence.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Models
+{
+    public class VendorExpence
+    {
+        public int Id { get; set; }
+
+        public int VendorId { get; set; }
+
+        public string Month { get; set; }
+
+        public decimal Ammount { get; set; }
+    }
+}
