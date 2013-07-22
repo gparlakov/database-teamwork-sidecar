a24e0134572ce7303f05d0269806093c5f4718e6
zhelyazkovn <zhelyazkovn@gmail.com>
2013-07-22 09:10:58 -0700

    Report Generator for xml and pdf created

---
+++ DBTeamWorkSidecar/ReportGenerator.cs/PDFGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket.DbContextSQL;
+using iTextSharp.text;
+using System.IO;
+using iTextSharp.text.pdf;
+using System.Data;
+
+namespace ReportGenerator.cs
+{
+    public class PDFGenerator
+    {
+        public void GeneratePDF(SupermarketDB sqlDbContext)
+        {
+            Console.WriteLine("Generating PDF Document...");
+
+            var query = sqlDbContext.Products.ToList();
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/testTablePDF5.pdf";
+
+            Document doc = new Document();
+            FileStream fs = File.Create(path);
+            PdfWriter.GetInstance(doc, fs);
+
+            doc.Open();
+
+            PdfPTable table = new PdfPTable(2);
+            var cell = new PdfPCell(new Phrase("TEST Tables"));
+            cell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            cell.Colspan = 2;
+            cell.HorizontalAlignment = 1;
+            table.AddCell(cell);
+
+            foreach (var item in query)
+            {
+                table.AddCell(item.ProductName);
+                table.AddCell(item.Vendor.ToString());
+            }
+
+            doc.Add(table);
+            doc.Close();
+
+        }
+    }
+}
---
+++ DBTeamWorkSidecar/ReportGenerator.cs/XMLGenerator.cs
@@ -0,0 +1,57 @@
+﻿using Supermarket.DbContextSQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ReportGenerator.cs
+{
+    public class XMLGenerator
+    {
+        public void GenerateXML(SupermarketDB sqlDbContext)
+        {
+            Console.WriteLine("Generatin XML....");
+
+            string fileName = "../../sales.xml";
+            Encoding encoding = Encoding.GetEncoding("windows-1251");
+
+
+            using (XmlTextWriter writer = new XmlTextWriter(fileName, encoding))
+            {
+                writer.Formatting = Formatting.Indented;
+                writer.IndentChar = '\t';
+                writer.Indentation = 1;
+
+                writer.WriteStartDocument();
+                writer.WriteStartElement("sales");
+
+                var query = sqlDbContext.Products.ToList();
+
+                foreach (var item in query)
+                {
+                    var data = DateTime.Now;
+                    WriteSale(writer, item.Vendor.VendorName.ToString(), data, item.BasePrice);
+                }
+
+                writer.WriteEndDocument();
+
+            }
+        }
+
+        private void WriteSale(XmlWriter writer, string vendor, DateTime date, decimal totalSum)
+        {
+            writer.WriteStartElement("sale");
+            writer.WriteAttributeString("vendor", vendor);
+
+            writer.WriteStartElement("summary");
+            writer.WriteAttributeString("date", date.ToString());
+            writer.WriteAttributeString("totalSum", totalSum.ToString());
+
+            writer.WriteEndElement();
+
+            writer.WriteEndElement();
+        }
+    }
+}
