780c312851f6055b2d51f528738d59b9b8cfa519
gparlakov <gparlakov@gmail.com>
2013-07-22 04:58:21 -0700

    Project - transfer data phase

---
+++ DBTeamWorkSidecar/Supermarket.Models/SaleByDate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Models
+{
+    public class SaleByDate
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        public int SupermarketId { get; set; }
+        public virtual Supermarket Supermarket { get; set; }
+
+        public float Quantity { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public decimal Sum { get; set; }
+    }
+}
---
+++ DBTeamWorkSidecar/Supermarket.TransferData/InitialTransferer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket.OpenAccess.Models;
+using Telerik.OpenAccess;
+using Supermarket.DbContextSQL;
+
+
+namespace Supermarket.TransferData
+{
+    public class InitialTransferer
+    {
+        static void Main()
+        {
+            using (var mySqlDbContext = new SupermarketDbMySQL())
+            {
+                TransferMeasures(mySqlDbContext);
+
+                TransferVendors(mySqlDbContext);
+
+                TransferProducts(mySqlDbContext);
+            }
+        }
+
+        private static void TransferMeasures(SupermarketDbMySQL mySqlDbContext)
+        {
+            var measures = mySqlDbContext.Measures.ToList();
+
+            
+            using (var sqlDbContext = new SupermarketDB())
+            {
+                
+                foreach (var measure in measures)
+                {                    
+                    sqlDbContext.Measures.Add(measure);//new Measure {Name = measure.MeasureName}                    
+                }
+
+                sqlDbContext.Database.ExecuteSqlCommand("SET IDENTITY_INSERT Supermarket.dbo.Measures ON");
+                sqlDbContext.Database.ExecuteSqlCommand("SET IDENTITY_INSERT Supermarket.dbo.Vendors ON");
+                sqlDbContext.Database.ExecuteSqlCommand("SET IDENTITY_INSERT Supermarket.dbo.Products ON");
+                sqlDbContext.SaveChanges();                
+            }
+        }
+
+        private static void TransferVendors(SupermarketDbMySQL mySqlDbContext)
+        {
+            var vendors = mySqlDbContext.Vendors.ToList();
+
+
+            using (var sqlDbContext = new SupermarketDB())
+            {
+                foreach (var vendor in vendors)
+                {
+                    sqlDbContext.Vendors.Add(vendor);
+                }
+
+                sqlDbContext.SaveChanges();
+            }
+        }
+
+        private static void TransferProducts(SupermarketDbMySQL mySqlDbContext)
+        {
+            var products = mySqlDbContext.Products.ToList();
+
+
+            using (var sqlDbContext = new SupermarketDB())
+            {
+                foreach (var product in products)
+                {
+                    sqlDbContext.Products.Add(product);
+                }
+
+                sqlDbContext.SaveChanges();
+            }
+        }
+    }
+}
