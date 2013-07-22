e2f3e0b3667d5e3cebefc4fbd9f734a286f165da
gparlakov <gparlakov@gmail.com>
2013-07-22 11:00:56 -0700

    Add model for TotalProductSales report

---
+++ DBTeamWorkSidecar/Supermarket.Models/TotalProductSales.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Models
+{
+    public class TotalProductSales
+    {
+        public ObjectId Id { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string VendorName { get; set; }
+        public float QuantitySold { get; set; }
+        public decimal TotalIncomes { get; set; }
+    }
+}
