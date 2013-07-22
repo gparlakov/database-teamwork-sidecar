014352def128e30026bf82d8d2f4f7ae0a35a1e3
gparlakov <gparlakov@gmail.com>
2013-07-22 12:22:13 -0700

    ReportsWriter SQL To MongoDb finished

--- DBTeamWorkSidecar/Supermarket.MongoDB.ReportsWriter/ReportsWriter.cs
+++ DBTeamWorkSidecar/Supermarket.MongoDB.ReportsWriter/ReportsWriter.cs
@@ -10,6 +10,7 @@ using MongoDB.Driver.GridFS;
 using MongoDB.Driver.Linq;
 using Supermarket.Models;
 using Supermarket.DbContextSQL;
+using Supermarket.OpenAccess;
 
 namespace Supermarket.MongoDBTest
 {
@@ -26,35 +27,44 @@ namespace Supermarket.MongoDBTest
         {
             using (var supermarketDB = new SupermarketDB())
             {
-                var productReports = supermarketDB.Products.Join(
+                //AllSalesReportsSqlToMongo(supermarketDB);
+
+                var server = MongoServer.Create(Local);
+                var db = server.GetDatabase(Database);
+                var collection = db.GetCollection<TotalProductSales>(CollectionReportsTest).FindAll();
+                
+                foreach (var item in collection)
+                {
+                    Console.WriteLine("{0} -> {1}",item.ProductName,item.QuantitySold);
+                }
             }
-            
-            
-            
-            
-            
-            
-            
-            
+        }
+
+        private static void AllSalesReportsSqlToMongo(SupermarketDB supermarketDB)
+        {
+            var productReports = supermarketDB.Database.SqlQuery<TotalProductSales>(@"
+                    SELECT  SUM(s.UnitPrice * s.Quantity) as TotalIncomes,
+		                    SUM(s.Quantity) as [QuantitySold],		
+		                    s.ProductId ,
+		                    MAX(p.ProductName) as ProductName,
+		                    MAX(v.VendorName) as VendorName
+                    FROM [Supermarket].[dbo].[SaleByDates] s
+	                    JOIN Products p ON s.ProductId = p.ID
+	                    JOIN Vendors v ON p.Vendor_ID = v.ID
+                    GROUP BY s.ProductId
+                    ORDER BY s.ProductId").ToList();
+
+
             var server = MongoServer.Create(Local);
 
-            var db = server[Database];
+            var db = server.GetDatabase(Database);
             if (!db.CollectionExists(CollectionReportsTest))
             {
                 db.CreateCollection(CollectionReportsTest);
             }
 
             var collection = db.GetCollection<TotalProductSales>(CollectionReportsTest);
-                        
-
-            //collection.InsertBatch();
-            
-            //var query = Query<TotalProductSales>.EQ(t => t.ProductName, "Tea");
-            //var inserted = collection.Find(query);
-            //foreach (var item in inserted)
-            //{
-            //    Console.WriteLine(item.ProductId); 
-            //}
+            collection.InsertBatch(productReports);
         }
     }
 }
