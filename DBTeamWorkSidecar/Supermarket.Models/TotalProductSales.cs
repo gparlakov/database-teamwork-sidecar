2ce086ee482bb3a0bde2b3c098f12eb9346aff5f
gparlakov <gparlakov@gmail.com>
2013-07-23 02:32:35 -0700

    TotalProductSales update

--- DBTeamWorkSidecar/Supermarket.Models/TotalProductSales.cs
+++ DBTeamWorkSidecar/Supermarket.Models/TotalProductSales.cs
@@ -1,16 +1,17 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Supermarket.Models
 {
     public class TotalProductSales
-    {
+    {        
         public ObjectId Id { get; set; }
+                
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string VendorName { get; set; }
